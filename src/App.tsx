0c33d58a72158e611e06bc7bcc650e5f38edd9c1
michaelm3 <michael.muniz94@gmail.com>
2023-01-01 16:43:56 -0500

    added array state for tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './App.css'
 import GameBoard from './components/GameBoard'
 
 const App: FC = () => {
+    const [turn, setTurn] = useState("X")
 
     return (
         <div className='App'>
-           <GameBoard /> 
+           <GameBoard turn={turn} setTurn={setTurn} /> 
         </div>
     )
 }
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,27 +1,43 @@
-import { FC, MouseEvent } from 'react'
+import { FC, MouseEvent, useState } from 'react'
 
-const GameBoard: FC = () => {
+interface IProps {
+    turn: string;
+    setTurn: (turn: string) => void;
+}
 
-    const placeShape = (e: MouseEvent<HTMLDivElement>) => {
-       (e.target as HTMLDivElement).innerText = "X"
+const GameBoard: FC<IProps> = ({ turn, setTurn }) => {
+    const [tiles, setTiles] = useState<string[]>(["", "", "", "", "", "", "", "", ""])
+
+    const placeShape = (e: MouseEvent) => {
+        const target = e.target as HTMLDivElement
+        if (checkValidMove(target.innerText)) {
+            const pos: number = +target.id as number
+            setTiles([...tiles.slice(0, pos), turn, ...tiles.slice(pos+1, 9)])
+            target.innerText = turn
+            turn === "X" ? setTurn("O") : setTurn("X")
+        }
+    }
+
+    const checkValidMove = (pos: string): boolean => {
+        return pos === "" ? true : false
     }
 
     return (
         <div className="board">
             <div className='row'>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
+                <div id="0" className='tile' onClick={placeShape}></div>
+                <div id="3" className='tile' onClick={placeShape}></div>
+                <div id="6" className='tile' onClick={placeShape}></div>
             </div>
             <div className='row'>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
+                <div id="1" className='tile' onClick={placeShape}></div>
+                <div id="4" className='tile' onClick={placeShape}></div>
+                <div id="7" className='tile' onClick={placeShape}></div>
             </div>
             <div className='row'>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
-                <div className='tile' onClick={placeShape}></div>
+                <div id="2" className='tile' onClick={placeShape}></div>
+                <div id="5" className='tile' onClick={placeShape}></div>
+                <div id="8" className='tile' onClick={placeShape}></div>
             </div>
         </div>
     )
