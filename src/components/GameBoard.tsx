f4631609739b4ff18b521599e2aaca90ecc55e81
michaelm3 <michael.muniz94@gmail.com>
2023-01-01 13:41:25 -0500

    basic css completed, and onclicks setup

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,23 +1,27 @@
-import { FC } from 'react'
-import GameTile from './GameTile'
+import { FC, MouseEvent } from 'react'
 
 const GameBoard: FC = () => {
+
+    const placeShape = (e: MouseEvent<HTMLDivElement>) => {
+       (e.target as HTMLDivElement).innerText = "X"
+    }
+
     return (
         <div className="board">
             <div className='row'>
-                <GameTile />
-                <GameTile />
-                <GameTile />
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
             </div>
             <div className='row'>
-                <GameTile />
-                <GameTile />
-                <GameTile />
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
             </div>
             <div className='row'>
-                <GameTile />
-                <GameTile />
-                <GameTile />
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
+                <div className='tile' onClick={placeShape}></div>
             </div>
         </div>
     )
